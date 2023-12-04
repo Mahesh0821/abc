119dd1e837e12ee22cb58c58f13ba9c92d4f18b4
Mahesh0821 <120931978+Mahesh0821@users.noreply.github.com>
2023-12-04 15:34:13 +0530

    Update projectcode.ino

--- projectcode.ino
+++ projectcode.ino
@@ -2,20 +2,23 @@
 #include <MFRC522.h>
 #include <ESP8266WiFi.h>
 #include <ThingSpeak.h>
+int LED=D1;
 
 // Define your WiFi credentials and ThingSpeak API key
 const char* ssid = "123";
 const char* password = "123456789";
-const char* apiKey = "AMST51W759VO5YIB";
+const char* apiKey = "L3RV786O394UI7S6";
+
 WiFiClient client;
 
 // Create an MFRC522 instance
-MFRC522 mfrc522(2, 4); // Define the SDA and RST pins
+MFRC522 mfrc522(2, 4);  // Define the SDA and RST pins
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(1200);
   SPI.begin();
   mfrc522.PCD_Init();
+  pinMode(LED,OUTPUT);
 
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
@@ -30,7 +33,7 @@ void setup() {
 
 
   // Define the fields for your ThingSpeak channel
-  int field1 = 0; // RFID Tag ID
+  int field1 = 0;  // RFID Tag ID
 
   // Set the fields
   ThingSpeak.setField(field1, 0);
@@ -47,10 +50,15 @@ void loop() {
 
     Serial.print("Tag UID: ");
     Serial.println(tagUID);
+    if (tagUID != 0) {
+      digitalWrite(D1, HIGH);
+      delay(500);
+      digitalWrite(D1, LOW);
 
-  
-    //Serial.println("Unauthorized Tag");
+    }
+
     sendDataToThingSpeak(0);
+
 
     mfrc522.PICC_HaltA();
     mfrc522.PCD_StopCrypto1();
@@ -58,13 +66,15 @@ void loop() {
 }
 
 void sendDataToThingSpeak(int tagID) {
-  int field1 = tagID; // Set the tag ID in field1
+  int field1 = tagID;  // Set the tag ID in field1
 
   // Update ThingSpeak fields
   ThingSpeak.setField(1, field1);
 
   // Write data to ThingSpeak
-  int response = ThingSpeak.writeFields(2335510, apiKey);
+  int response = ThingSpeak.writeFields(2367912, apiKey);
+  Serial.println(response);
+
 
   if (response == 200) {
     Serial.println("Data sent to ThingSpeak successfully!");
@@ -72,6 +82,6 @@ void sendDataToThingSpeak(int tagID) {
     Serial.print("Error sending data to ThingSpeak. HTTP error code: ");
     Serial.println(response);
   }
-   response=0;
-  delay(1000); // Update data every 5 seconds
+  response = 0;
+  delay(1000);  // Update data every 5 seconds
 }
